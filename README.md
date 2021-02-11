fbe6aca02faf81d9a554955380536bdf022e10aa
Kanishk Navale <navalekanishk@gmail.com>
2021-02-11 16:28:34 +0100

    Final Wrap

--- README.md
+++ README.md
@@ -2,17 +2,27 @@
 Tensorflow based DQN Agent for 'MountainCar-V0' openai gym environment
 
 ## Table of contents
-* DQN class
+* DQN class algorithm based on,
+    <p ><img src="https://wiki.math.uwaterloo.ca/statwiki/images/9/9e/QLearning_Alg.JPG" width="450" ></p>
 * State-Space Surface Plot
+
+## About the DQN Agent
+* The agent reached the goal position at epoch: 17
+* The agent policy begins to converge with no fail scenario from epoch:164 
+* Results,
+    * Value Function Plot,
+    <p ><img src="Value_function.png" width="500" ></p>
+    * Accumulated Reward Plot,
+    <p ><img src="Reward Plot.png" width="500" ></p>
+    * Episode Length Plot,
+    <p ><img src="Episode Length Plot.png" width="500" ></p>
 
 ## Dependencies
 Install dependencies using:
 ```bash
 pip3 install -r requirements.txt 
 ```
-
 ## Challenge
-* Reduce the convergence time
 * Achieve the goal position with optimum episode length
 
 ## Contact
--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from collections import deque
 import random
 import numpy as np
 
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
 import tensorflow as tf
 from tensorflow.keras import models
 from tensorflow.keras.layers import Dense
@@ -28,6 +31,7 @@ class DQN_Agent:
         self.epsilon_decay= 0.05
         self.epsilon_min= 0.01
         self.learning_rate= 0.001
+        self.best_score = -np.Inf
         
         self.replay_memory= deque(maxlen= 20000)
         self.buffer_size= 32
@@ -54,7 +58,7 @@ class DQN_Agent:
         state_shape = self.env.observation_space.shape
 
         model.add(Dense(16, activation='relu', input_shape=state_shape))
-        model.add(Dense(32, activation='relu'))
+        model.add(Dense(64, activation='relu'))
         model.add(Dense(16, activation='relu'))
         model.add(Dense(self.env.action_space.n, activation='linear'))
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
@@ -67,7 +71,7 @@ class DQN_Agent:
         self.epsilon= max(self.epsilon_min, self.epsilon)
 
         if np.random.rand(1) < self.epsilon:
-            action= np.random.randint(0, 3)
+            action= self.env.action_space.sample()
         else:
             action= np.argmax(self.train_network.predict(state)[0])
 
@@ -113,22 +117,15 @@ class DQN_Agent:
 
 
     ##############################################################################
-    def run(self,current_state,eps):
+    def run(self, eps):
         """ Generate Episodic Data """
         
         sum_reward= 0
         done= False
+        current_state= self.env.reset().reshape(1,2)
         
         for i in range(self.len_episode):
             opt_action= self.epsgreedyaction(current_state)
-
-            # Render the env. at the last iteration
-            if (eps % self.n_episodes) == 0:
-                try:
-                    self.env.render()
-                except:
-                    pass
-
             new_state, reward, done, _= self.env.step(opt_action)
             new_state = new_state.reshape(1, 2)
                 
@@ -143,19 +140,21 @@ class DQN_Agent:
             current_state= new_state
 
             if done:
+                self.env.close()
                 break
 
         if sum_reward > -200:
-            print(f'Episode: {eps} Status: SUCCESS! Episode Time: {self.len_episode} with Reward: {sum_reward} & Exploration Rate: {self.epsilon}')
+            print(f'Episode: {eps} Status: SUCCESS! with Reward: {sum_reward}')
+            if sum_reward >= self.best_score:
+                self.train_network.save('Trained_Model.h5')
+                self.best_score = sum_reward
+                print('Best Reward Received till now!')
         else:
             print(f'Episode: {eps} Status: FAIL! with Exploration Rate: {np.around(self.epsilon,4)}')
             
-        if eps == self.n_episodes-1:
-            tf.saved_model.save(self.train_network, 'trained_network.h5')
-            
         # Transfer the weights to 'target_network'
         self.target_network.set_weights(self.train_network.get_weights())
-
+ 
         # Exploration Decay
         self.epsilon -= self.epsilon_decay
         
@@ -165,13 +164,92 @@ class DQN_Agent:
         """ Initiate gameplay """
         
         for episode in range(self.n_episodes):
-            current_state= self.env.reset().reshape(1,2)
-            self.run(current_state, episode)
+            self.run(episode)
+
+            
+##############################################################################
+# Analytics
+##############################################################################
+def analytics(epochs, env, model):
+    """ Gather data for analyzing the model """
+    
+    log_reward= []
+    log_episode= []
+    log_position= []
+    log_velocity= []
+    
+    for i in range(epochs+1):
+        sum_reward= 0
+        sum_episode_length = 0
+        done= False
+        current_state= env.reset().reshape(1,2)
+        print (f'Running Iteration: {i}')
+        
+        for i in range(201):
+            try:
+                env.render()
+            except:
+                pass
+            
+            opt_action= np.argmax(model.predict(current_state)[0])
+            new_state, reward, done, _= env.step(opt_action)
+            new_state = new_state.reshape(1, 2)
+
+            sum_reward+= reward
+            current_state= new_state
+            sum_episode_length+= 1
+
+            if done:
+                env.close()
+                break
+                
+            log_position.append(current_state[0][0])
+            log_velocity.append(current_state[0][1])
+        
+        log_reward.append(sum_reward)
+        log_episode.append(sum_episode_length)
+        
+        # Generate Value Plot
+        plt.figure(1)
+        plt.xlabel('Position')
+        plt.ylabel('Velocity')
+        plt.hist2d (log_position, log_velocity)
+        string = 'Value Function at Episode:'+str(i)
+        plt.title(string)
+        plt.savefig('Value_function.png')
+        
+        # Reward Performance Plot
+        plt.figure(2)
+        plt.plot(log_reward)
+        plt.ylabel('Accumulated Reward')
+        plt.xlabel('n_episodes')
+        plt.legend(loc='best')
+        plt.title('Accumulated Reward Plot')
+        plt.savefig('Reward Plot.png')
+        
+        # Episode Length Performance Plot
+        plt.figure(3)
+        plt.plot(log_episode)
+        plt.ylabel('Episode Length')
+        plt.xlabel('n_episodes')
+        plt.legend(loc='best')
+        plt.title('Episode Length Plot')
+        plt.savefig('Episode Length Plot.png')
+        
 
 
 ##############################################################################
 # Main!
 ##############################################################################
 if __name__ == "__main__":
+    # Train the DQN Agent
     mountain_car= DQN_Agent(env= gym.make('MountainCar-v0'))
     mountain_car.train()
+    
+    # Load the Model
+    Agent = tf.keras.models.load_model('./Trained_Model.h5')
+    
+    # Analytics of the DQN Agent
+    analytics(50, mountain_car.env, Agent)
+    
+    
